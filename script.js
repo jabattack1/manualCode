4d9e659cce10ecbabc59bb7bfeacb0cc5a43e977
jabattack1 <peter.hahn@live.com>
2019-04-22 21:30:47 -0400

    manualCodes

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//ajax call to get data and iterate though it to append html with data
+$('.comment-list').html('Loading...');
+
+$.ajax({
+        type: 'GET',
+        url:'./data/users.json',
+        data: JSON.stringify,
+        dataType: "json",
+        success:function(data){
+            console.log('success');
+
+            setTimeout(function(){
+            $('.comment-list').html(data).css("display", "block");
+            $.each(data.users, function(index, element) {
+                var html = '<div class=\'comment-item_username\'>'+element.Name+'</div>';
+                    html += '<div class=\'comment-item_message\'>'+element.Message+'</div>';
+            $('.comment-list').append(html); 
+            });
+            },1000);
+        },
+        error : function(e) {
+            console.info("Error");
+        },
+        done : function(e) {
+            console.info("DONE");
+        }
+});
+
+
+
 //remove all instances of value
 Array.prototype.remove = function() {
      var what, a = arguments, L = a.length, ax;
@@ -33,3 +63,31 @@ Array.prototype.remove = function() {
     }
     return this;
 };
+
+
+
+// return matching values in multiple arrays
+  var result = newArray.shift().filter(function(v) {
+        return newArray.every(function(a) {
+            return a.indexOf(v) !== -1;
+        });
+    });
+
+
+
+// get total of array
+var total = nums.reduce((a, b) => a + b, 0)
+
+
+
+
+//get only unique arrays of an array
+[...new Set(final.map(v => JSON.stringify(v)))].map(v => JSON.parse(v));
+
+
+
+
+
+
+
+
