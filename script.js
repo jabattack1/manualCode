a3186de10bfcc9fda241db0749def13e93632ec0
jabattack1 <peter.hahn@live.com>
2019-04-24 22:23:52 -0400

    manual

--- script.js
+++ script.js
@@ -86,7 +86,40 @@ var total = nums.reduce((a, b) => a + b, 0)
 
 
 
+var letterCombinations = function(digits) {
+    if (!digits) {
+        return [];
+    }
+    
+    const mapping = [
+        ['a','b','c'],
+        ['d','e','f'],
+        ['g','h','i'],
+        ['j','k','l'],
+        ['m','n','o'],
+        ['p','q','r','s'],
+        ['t','u','v'],
+        ['w','x','y','z'],
+    ];
+    
+    const letters = digits.split('').map(digit => mapping[parseInt(digit - 2)]);
 
+    return combine(letters, 0, '');
+};
+
+const combine = (letters, index, current) => {
+    if (letters.length === index) {
+        return [current];
+    }
+    
+    const combinations = [];
+    
+    letters[index].forEach((letter) => {
+        combinations.push(...combine(letters, index + 1, `${current}${letter}`));
+    });
+    console.log(combinations);
+    return combinations;
+};
 
 
 
